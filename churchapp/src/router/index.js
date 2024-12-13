2d85374abfe083ccaf751bdf6d5bd867aa74a6cf
Leticiamkabu <kabuleticia36@gmail.com>
2024-12-13 10:00:11 +0000

    changed to has mode

--- churchapp/src/router/index.js
+++ churchapp/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 // import App from '../App.vue';
 import WelcomePage from '../components/WelcomePage.vue';
 import LoginPage from '../components/LoginPage.vue';
@@ -35,7 +35,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
