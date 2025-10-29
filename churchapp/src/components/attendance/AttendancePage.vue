1073a42c96c449938c940bb5b9e1db9aa0b939f4
Leticia Mamley Kabu <leticia@Leticias-MacBook-Pro.local>
2025-10-29 13:05:40 +0000

    aded new implemetation changes

--- churchapp/src/components/attendance/AttendancePage.vue
+++ churchapp/src/components/attendance/AttendancePage.vue
@@ -140,7 +140,7 @@ console.info("selected department name : ", this.selectedDepartment);
   try {
     // https://churchmsbackend.onrender.com
 
-    const response = await axios.get(`http://localhost:8000/members/get_member_by_words/${this.name}/${this.selectedDepartment}`);
+    const response = await axios.get(`https://churchmsbackend.onrender.com/members/get_member_by_words/${this.name}/${this.selectedDepartment}`);
     console.info(response); 
 
     // Check if members were found
